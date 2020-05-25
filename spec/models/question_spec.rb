0c5407ede95983348519dbe1cb9b359946d452d8
malovevgen <malovevgen@yandex.ru>
2020-05-26 02:05:00 +0300

    shoulda tests

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,15 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  it 'validates presece of title' do
-    expect(Question.new(body: '123')).to_not be_valid
+  it { should validate_presence_of :title }
+  it { should validate_presence_of :body }
+  # it 'validates presece of title' do
+    # expect(Question.new(body: '123')).to_not be_valid
     # question = Question.new(body: '123')
     # expect(question).to_not be_valid
     # pending "add some examples to (or delete) #{__FILE__}"
     # expect(question.valid?).to be_falsey
-  end
+  # end
 
-  it 'validates presece of body' do
-    expect(Question.new(title: '123')).to_not be_valid
-  end
+  # it 'validates presece of body' do
+    # expect(Question.new(title: '123')).to_not be_valid
+  # end
 end
