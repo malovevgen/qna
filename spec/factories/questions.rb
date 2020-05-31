6897af1cf9e02c89d4a7e792644ba4fd73c5e42a
malovevgen <malovevgen@yandex.ru>
2020-05-31 21:05:56 +0300

    fixed a bug in factories

--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -2,9 +2,9 @@ FactoryBot.define do
   factory :question do
     sequence(:title) { |n| "MyString #{n}" }
     sequence(:body) { |n| "MyText #{n}" }
-  end
 
-  trait :invalid do
-    title { nil }
+    trait :invalid do
+      title { nil }
+    end
   end
 end
