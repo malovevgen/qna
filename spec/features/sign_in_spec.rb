9e4238ca48513d0b7533d10bb48db86871d70b80
malovevgen <malovevgen@yandex.ru>
2020-06-13 22:19:58 +0300

    webpack and yarn

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -5,15 +5,23 @@ feature 'User can sign in', %q{
   as an unaunthenticated User
   I'd like to be able to sign in
 } do
-  scenario 'Registered user tries to sign in' do
-    User.create!(email: 'user@test.com', password: '12345678')
 
-    fill_in 'Email', with: 'user@test.com'
-    fill_in 'Password', with: '12345678'
+  given(:user) { User.create!(email: 'user@test.com', password: '12345678') }
+
+  background { visit new_user_session_path }
+  scenario 'Registered user tries to sign in' do
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_on 'Log in'
 
     expect(page).to have_content 'Signed in successfully'
-
   end
-  scenario 'Unregistered user tries to sign in'
-  
+
+  scenario 'Unregistered user tries to sign in' do
+    fill_in 'Email', with: 'wrong@test.com'
+    fill_in 'Password', with: '12345678'
+    click_on 'Log in'
+
+    expect(page).to have_content 'Invalid Email or password'
+  end  
 end
