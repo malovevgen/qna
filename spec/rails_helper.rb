28a1ff6a836e3f140a46efbb82e9871877173afc
malovevgen <malovevgen@yandex.ru>
2020-06-30 23:27:31 +0300

    refactoring spec29:25

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -15,6 +15,7 @@ RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include ControllerHelpers, type: :controller
+  config.include FeatureHelpers, type: :feature
   
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
